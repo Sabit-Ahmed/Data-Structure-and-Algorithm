9f7cdb922dc1850ffd21b8d85ffd08784ebea6c3
Sabit-Ahmed <sabit.a.sirat@gmail.com>
2022-06-28 12:07:15 +0600

    implement insert function for BST

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -1,2 +1,36 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
 
 class BinarySearchTree:
+
+    def insert(self, node, key) -> Node:
+        if node is None:
+            return Node(key)
+        if key < node.key:
+            node.left = self.insert(node.left, key)
+        else:
+            node.right = self.insert(node.right, key)
+        if node.right is not None:
+            print(node.right.key)
+        return node
+
+
+def main():
+    bst = BinarySearchTree()
+    root = None
+    root = bst.insert(root, 8)
+    root = bst.insert(root, 3)
+    root = bst.insert(root, 1)
+    root = bst.insert(root, 6)
+    root = bst.insert(root, 7)
+    root = bst.insert(root, 10)
+    root = bst.insert(root, 14)
+    root = bst.insert(root, 4)
+    print(root.key)
+
+if __name__ == "__main__":
+    main()
